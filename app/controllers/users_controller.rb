5d1e0feee25f9a7d74edf762cdf29687341bd816
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-07-08 12:17:47 -0700

    added login forms, get and post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,27 @@ class UsersController < ApplicationController
     @user = User.create(params[:user])
       if @user.save
         session[:user_id] = @user.id
-        redirect to '/events'
+        redirect to '/event'
       else
         redirect '/signup'
       end
-
   end
 
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @users = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/events'
+    else
+      erb :'/login'
+    end
+
+  end
 
 
 
