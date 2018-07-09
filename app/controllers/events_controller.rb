936f703869acb4a8f6b88e33f660c0acdac0b01d
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-07-08 20:49:38 -0700

    added create, edit, show, events file to events folder

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,19 @@
 class EventsController < ApplicationController
 
   get '/events' do
-    erb :'events/events'
+    if !logged_in?
+      redirect '/login'
+    else
+      erb :'events/events'
+    end
+  end
+
+  get '/events/new' do
+    if !logged_in?
+      redirect '/login'
+    else
+      erb :'events/create_event'
+    end
   end
 
 end
