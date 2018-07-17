1716dfaa6c5a3d437aad3a5da0b2ab3fa9661add
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-07-16 21:21:56 -0700

    deleted favicon - kept set up for future favicon

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,12 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :events
   has_secure_password
   validates :username, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
-  has_many :events
 
-  include Slugify::InstanceMethods
-    extend Slugify::ClassMethods
 
 
 end
